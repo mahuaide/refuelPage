304a206cc3a3d8a6418b52491d2f26e39f36f91b
mahd <mahd@asiainfo.com>
2019-06-29 09:05:55 +0800

    看板demo

--- src/components/complexKanbanHeaderCustom.vue
+++ src/components/complexKanbanHeaderCustom.vue
@@ -2,17 +2,23 @@
   <div class="header">
     <table cellspacing="0" cellpadding="0">
       <tbody>
+      <!-- 表头采用table单行模式 -->
       <tr>
+        <!-- 按顶级节点循环，每个顶级节点作为一列 -->
         <template v-for="(level_1,index) in lane">
           <td>
             <tr>
+              <!-- 一级表头的列合并，取决于有多少个最底层元素，具体算法在父组件watch里面 -->
               <td :colspan="level_1.deepNum">{{level_1.label}}</td>
             </tr>
             <tr>
+              <!-- 二级表头需要对列和行分别合并 -->
                <template v-for="(level_2,index) in level_1.children">
+                 <!-- 如果二级表头无子节点，需要向下合并一行 -->
                  <template v-if="!level_2.children || level_2.children.length ==0">
                    <td rowspan="2">{{level_2.label}}</td>
                  </template>
+                 <!-- 如果二级表头有子节点，需要向右合并，有多少子节点合并多少列 -->
                  <template v-else>
                    <td :colspan="level_2.children.length">{{level_2.label}}</td>
                  </template>
@@ -31,7 +37,6 @@
       </tbody>
     </table>
   </div>
-
 </template>
 
 <script type="text/ecmascript-6">
