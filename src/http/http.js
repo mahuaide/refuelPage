12f7136149095082555d28f66d4fecc4108703c6
mahuaide <mhd_001@sina.com>
2019-12-30 22:45:54 +0800

    1

--- src/http/http.js
+++ src/http/http.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Created by Administrator on 2017/3/20.
  */
 import axios from 'axios'
@@ -9,7 +9,7 @@ axios.defaults.timeout = 60000;
 // axios.defaults.baseURL = "http://refuel.applinzi.com";
 // axios.defaults.baseURL = "http://k2321512s9.imwork.net:16607";
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = "http://k2321512s9.imwork.net:16607";
+  axios.defaults.baseURL = "http://k2321512s9.imwork.net:50387";
   // axios.defaults.baseURL = "http://localhost:3000";
 }else{
   axios.defaults.baseURL = "http://localhost:3000";
